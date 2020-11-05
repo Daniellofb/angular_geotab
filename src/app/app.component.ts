a5d3e43b2e04467f8c6263c51f85f743c654603d
Daniel Lopez Oliveros <dannylo9891@gmail.com>
2020-11-05 14:34:07 -0600

    Fix se quitaron el uso de cors para el manejo de peticiones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit {
     description: 'Dos',
     published: false
   };
-  public submitted = false;
+  public submitted = true;
 
   private urlapi = 'https://attcflotilla236.geotab.com/apiv1';
   private result = new Date().toISOString();
@@ -76,6 +76,20 @@ export class AppComponent implements OnInit {
       this.httpClient.post(this.urlapi, payloadObject).subscribe(
         data => this.resultData = data['result'][0]['data']
       );
+      const data = {
+        title: this.demo.title,
+        description: this.resultData
+      };
+  
+      this.tutorialService.create(data)
+        .subscribe(
+          response => {
+            console.log(response);
+            this.submitted = true;
+          },
+          error => {
+            console.log(error);
+          });
       console.log(this.resultData);
     }, 20000)
     // }
