b1248679fdc19e1d37eadaaa6dbee4184d77ca25
DanielFKB <daniel@isinet.ws>
2020-11-05 13:40:58 -0600

    Fix - Salvar datos

--- src/app-routing.module.ts
+++ src/app-routing.module.ts
@@ -3,8 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app/app.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'tutorials', pathMatch: 'full' },
-  { path: 'add', component: AppComponent }
+  { path: '', component: AppComponent }
 ];
 
 @NgModule({
